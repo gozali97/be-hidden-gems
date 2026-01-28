7c831a54b8259b3b5a2e0a64f38b4330c305462f
Ahmad Gozali <ahmadmulti10@gmail.com>
2026-01-28 10:59:40 +0700

    Fix Vercel config: switch to api/index.js and remove legacy builds

--- api/index.js
+++ api/index.js
@@ -1,19 +1,19 @@
 const strapi = require('@strapi/strapi');
 
-// Instance Strapi singleton
 let strapiInstance;
 
 module.exports = async (req, res) => {
-    // Initialize Strapi if not already running (Cold Start)
+    // Cold start protection
     if (!strapiInstance) {
+        // Vercel menjalankan file ini dari root, jadi kita perlu menunjuk ke direktori yang benar
         strapiInstance = await strapi.createStrapi({
-            appDir: __dirname,
-            distDir: __dirname + '/dist',
+            appDir: process.cwd(),
+            distDir: process.cwd() + '/dist',
         }).load();
 
         await strapiInstance.server.mount();
     }
 
-    // Forward request to Strapi's Koa server
+    // Forward request to Strapi
     strapiInstance.server.httpServer.emit('request', req, res);
 };
