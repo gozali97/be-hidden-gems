d1f56027760199f1f1b50a42748376441e8a9c9b
Ahmad Gozali <ahmadmulti10@gmail.com>
2026-01-28 11:48:06 +0700

    Support DATABASE_URL for Strapi Cloud

--- config/database.ts
+++ config/database.ts
@@ -24,6 +24,7 @@ export default ({ env }) => {
     },
     postgres: {
       connection: {
+        connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
